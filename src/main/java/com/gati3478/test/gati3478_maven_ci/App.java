727e27521ce5dca6cf4488d4a6188e5408379f12
gati3478 <giopetriashvili@gmail.com>
2015-04-26 12:56:45 +0400

    Added initial project files

---
+++ src/main/java/com/gati3478/test/gati3478_maven_ci/App.java
@@ -0,0 +1,13 @@
+package com.gati3478.test.gati3478_maven_ci;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
